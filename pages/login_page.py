26f512b2ccdd08a1bc75fdcd44997ff365bd3068
jhon2rrrr <168171282+jhon2rrrr@users.noreply.github.com>
2025-05-04 12:28:43 +0300

    Create login_page.py

---
+++ pages/login_page.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.common.by import By
+
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.url = "https://berpress.github.io/selenium-login-demo/"
+        self.username_input = (By.ID, "username")
+        self.password_input = (By.ID, "password")
+        self.login_button = (By.ID, "login-btn")
+        self.result_div = (By.ID, "result")
+
+    def open(self):
+        self.driver.get(self.url)
+
+    def login(self, username, password):
+        self.driver.find_element(*self.username_input).send_keys(username)
+        self.driver.find_element(*self.password_input).send_keys(password)
+        self.driver.find_element(*self.login_button).click()
+
+    def get_result_text(self):
+        return self.driver.find_element(*self.result_div).text
+
+    def is_result_displayed(self):
+        return self.driver.find_element(*self.result_div).is_displayed()
